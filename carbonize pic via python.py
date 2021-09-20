b1e341c2ad5f50de15847e7970a7a11062d3ccb4
Gaurav Kumar <50291149+Gauravkumario@users.noreply.github.com>
2021-09-20 22:45:47 +0530

    Add files via upload

---
+++ carbonize pic via python.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+import numpy as np
+
+#reading images
+img = cv2.imread("Desktop\gaurav.jpg")
+
+#edegs
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 5)
+edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY, 9, 9)
+
+#cartoonization
+color = cv2.bilateralFilter(img, 9, 250, 250)
+cartoon = cv2.bitwise_and(color, color, mask=edges)
+cv2.imshow("Image", img)
+cv2.imshow("edeges", edges)
+cv2.imshow("Cartoon", cartoon)
+cv2.waitKey(0)
+cv2.destroyALLWindows()
+
+
+# In[ ]:
+
+
+
+
